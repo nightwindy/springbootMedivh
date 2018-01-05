e9b11c67dadc687aa7c7ee4e2b95e6b72ae2cea2
windy <qiyu@maihaoche.com>
2018-01-05 17:22:17 +0800

    update

---
+++ medivh-core/src/main/java/com/windy/medivh/core/MedivhApplication.java
@@ -0,0 +1,34 @@
+package com.windy.medivh.core;
+
+//import com.camaro.starter.mq.annotation.EnableMQConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author songshuang
+ * @Date 2017-11-03
+ */
+@SpringBootApplication
+@ComponentScan("com.windy.medivh.core")
+@EntityScan("com.windy.medivh.dao.entity")
+@EnableJpaRepositories("com.windy.medivh.dao.repository")
+@RestController
+@EnableScheduling
+/*@EnableMQConfiguration*/
+@EnableDiscoveryClient
+public class MedivhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedivhApplication.class, args);
+    }
+
+
+
+
+}
