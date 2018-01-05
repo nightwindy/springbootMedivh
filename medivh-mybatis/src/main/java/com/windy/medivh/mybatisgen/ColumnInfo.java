a5b5164cb695acb6ca314797be05902663cec964
windy <qiyu@maihaoche.com>
2018-01-05 15:48:11 +0800

    update

---
+++ medivh-mybatis/src/main/java/com/windy/medivh/mybatisgen/ColumnInfo.java
@@ -0,0 +1,49 @@
+package com.windy.medivh.mybatisgen;
+
+public class ColumnInfo {
+    private String name;
+    private String calssName;
+    private String typeName;
+    private String precision;
+    private String scale;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCalssName() {
+        return calssName;
+    }
+
+    public void setCalssName(String calssName) {
+        this.calssName = calssName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(String precision) {
+        this.precision = precision;
+    }
+
+    public String getScale() {
+        return scale;
+    }
+
+    public void setScale(String scale) {
+        this.scale = scale;
+    }
+}
---
+++ medivh-mybatis/src/main/java/com/windy/medivh/mybatisgen/Main.java
@@ -0,0 +1,34 @@
+package com.windy.medivh.mybatisgen;
+
+
+import org.apache.commons.collections.CollectionUtils;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * mybatisgen main.
+ *
+ * @author windy.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        // 需要生成的表配置文件在generate_tables.config文件中，每每行一个表名
+        URL url = Thread.currentThread().getContextClassLoader().getResource("generate_tables.config");
+        if (url != null) {
+            String path = url.getPath();
+            List<String> tables = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+            if (CollectionUtils.isEmpty(tables)) {
+                System.out.println("generate_tables.config is empty.");
+                return;
+            }
+            MyBatisGenCore.batchGen(tables);
+            System.out.print("all done");
+        }
+    }
+}
